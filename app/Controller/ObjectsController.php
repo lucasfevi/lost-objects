4e7007c72e605f45a1ec666d0f90be15242bafaf
Lucas Vieira <newlucasfevi@gmail.com>
2015-12-01 11:39:49 -0600

    Removed message not being used

--- app/Controller/ObjectsController.php
+++ app/Controller/ObjectsController.php
@@ -17,7 +17,6 @@ class ObjectsController extends AppController
             $this->Objects->create();
 
             if ($this->Objects->save($this->request->data)) {
-                $this->Flash->success(__('Your account has been created'), array('key' => 'success'));
                 return $this->redirect(array('controller' => 'users', 'action' => 'view', 'userId' => CakeSession::read('Auth.User.id')));
             }
 
